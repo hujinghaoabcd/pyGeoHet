751a99cf545f72d90714b0cfe63ddb1a2e15eb97
Jinghao Hu <731409213@qq.com>
2026-08-05 03:26:54 +0800

    Stage 3B: export spatial-scale workflow

--- src/pygeohet/models/__init__.py
+++ src/pygeohet/models/__init__.py
@@ -2,11 +2,23 @@
 
 from pygeohet.models.geodetector import GeoDetector, geodetector
 from pygeohet.models.opgd import OPGD, OPGDResult, opgd
+from pygeohet.models.spatial_scale import (
+    SpatialScaleCandidateResult,
+    SpatialScaleOPGD,
+    SpatialScaleOPGDResult,
+    SpatialScaleResult,
+    compare_spatial_scales,
+)
 
 __all__ = [
     "GeoDetector",
     "OPGD",
     "OPGDResult",
+    "SpatialScaleCandidateResult",
+    "SpatialScaleOPGD",
+    "SpatialScaleOPGDResult",
+    "SpatialScaleResult",
+    "compare_spatial_scales",
     "geodetector",
     "opgd",
 ]
