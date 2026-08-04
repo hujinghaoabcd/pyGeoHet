8125bf705e0a728da35d1cb1c1302d17bb260135
Jinghao Hu <731409213@qq.com>
2026-08-05 04:09:11 +0800

    feat: export MSD public stratification API

--- src/pygeohet/stratification/__init__.py
+++ src/pygeohet/stratification/__init__.py
@@ -1,6 +1,13 @@
 """Continuous-variable stratification and q-guided optimization."""
 
 from pygeohet.stratification.methods import canonical_method, stratify
+from pygeohet.stratification.msd import (
+    MSD,
+    MSDResult,
+    MSDScaleResult,
+    MSD_TIE_RULE,
+    multiscale_discretize,
+)
 from pygeohet.stratification.optimal import (
     TIE_RULE,
     evaluate_stratification,
@@ -13,12 +20,17 @@ from pygeohet.stratification.results import (
 )
 
 __all__ = [
+    "MSD",
+    "MSDResult",
+    "MSDScaleResult",
+    "MSD_TIE_RULE",
     "TIE_RULE",
     "OptimalStratificationResult",
     "StratificationEvaluationResult",
     "StratificationResult",
     "canonical_method",
     "evaluate_stratification",
+    "multiscale_discretize",
     "optimize_stratification",
     "stratify",
 ]
