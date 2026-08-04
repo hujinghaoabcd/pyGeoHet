303af9b62da56a37f75e51741e0efafc51728bb6
Jinghao Hu <731409213@qq.com>
2026-08-05 05:23:23 +0800

    feat: export spatial variance API

---
+++ src/pygeohet/spatial/__init__.py
@@ -0,0 +1,5 @@
+"""Spatial-weight statistics used by SSH extensions."""
+
+from pygeohet.spatial.variance import SpatialVarianceResult, spatial_variance
+
+__all__ = ["SpatialVarianceResult", "spatial_variance"]
