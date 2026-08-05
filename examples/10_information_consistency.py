fafb0428f0254281090786a43047630a7555eb5f
Jinghao Hu <731409213@qq.com>
2026-08-05 16:33:42 +0800

    docs: complete information-consistency example

--- examples/10_information_consistency.py
+++ examples/10_information_consistency.py
@@ -25,4 +25,29 @@ continuous_target = np.asarray(
     [0.0, 0.2, 0.8, 1.0, 5.0, 5.2, 5.8, 6.0] * 3,
     dtype=float,
 )
-continuous_strata = np.asarray(["west"] * 4 + ["east"] * 4) repeat = None
+continuous_strata = np.tile(["west"] * 4 + ["east"] * 4, 3)
+continuous = continuous_information_consistency(
+    continuous_target,
+    continuous_strata,
+    bins=4,
+    permutations=99,
+    random_state=42,
+)
+print(continuous.summary())
+print(continuous.contributions_frame().to_string(index=False))
+print()
+
+factors = pd.DataFrame(
+    {
+        "separated": continuous_strata,
+        "mixed": np.tile(["west", "east"] * 4, 3),
+    }
+)
+workflow = InformationConsistency(
+    target_kind="continuous",
+    bins=4,
+    permutations=49,
+    random_state=42,
+).fit(continuous_target, factors)
+print(workflow.summary())
+print(workflow.to_frame().to_string(index=False))
